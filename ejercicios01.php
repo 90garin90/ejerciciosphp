05828c638055742c3b6771e101ce07230d4af398
Alejandro Garin <garin_123@hotmail.com>
2016-09-30 18:19:08 +0200

    prueba2

--- ejercicios01.php
+++ ejercicios01.php
@@ -1,1 +1,19 @@
-TODO:
+<!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>ejercicios01</title>
+</head><body>
+<?php
+//inicializamos
+echo "Array ordenado inicializado en la declaración <hr>";
+$array1 = array (
+    1,2,3,4,5
+);
+
+echo "<hr> Recorrido mediante bucle foreach simple <br>";
+foreach ($array1 as $element){
+    echo $element . '<br>';
+}
+?>
+</body>
+</html>
